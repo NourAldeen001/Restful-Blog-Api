62f88774f374ff2884931a9217dc445b708d9fc5
NourAldeen001 <118866955+NourAldeen001@users.noreply.github.com>
2026-04-09 22:52:42 +0200

    initial commit

---
+++ src/main/java/com/master/Restful_Blog_Api/RestfulBlogApiApplication.java
@@ -0,0 +1,13 @@
+package com.master.Restful_Blog_Api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestfulBlogApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestfulBlogApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/master/Restful_Blog_Api/RestfulBlogApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.master.Restful_Blog_Api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestfulBlogApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
